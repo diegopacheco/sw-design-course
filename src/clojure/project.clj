6a86d2218c1305cde188657eb009691a6e600adc
Diego Pacheco <diego.pacheco.it@gmail.com>
2019-11-05 21:22:28 -0800

    added pattern matcher lib

--- src/clojure/project.clj
+++ src/clojure/project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/diegopacheco/sw-design-course"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"],[org.clojure/core.match "0.3.0"]]
   :main helloworld)
