315fdff2aa6e2496f1e171c164edaecaaf0a4cd5
Diego Pacheco <diego.pacheco.it@gmail.com>
2019-11-05 19:19:07 -0800

    samples with clojure + .gitignore

---
+++ src/clojure/project.clj
@@ -0,0 +1,7 @@
+(defproject helloworld "1.0.0-SNAPSHOT"
+  :description "Hello World Project"
+  :url "https://github.com/diegopacheco/sw-design-course"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :main helloworld)
---
+++ src/clojure/src/helloworld.clj
@@ -0,0 +1,4 @@
+(ns helloworld)
+(defn -main                   ; function name
+  [& args]                    ; function args
+  (println "Hello, World!"))  ; function code
