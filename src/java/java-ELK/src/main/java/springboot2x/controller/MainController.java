e5ca36b749db57770a1ed83fbc0d5709786f5ce1
Diego Pacheco <diego.pacheco.it@gmail.com>
2020-01-02 18:07:05 -0300

    Added loback+logstash support for sending logs to ELK

--- src/java/java-ELK/src/main/java/springboot2x/controller/MainController.java
+++ src/java/java-ELK/src/main/java/springboot2x/controller/MainController.java
@@ -1,13 +1,18 @@
 package springboot2x.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController{
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
+
     @RequestMapping("/")
     public String index() {
+      LOGGER.info("Serving a success request. Dont do this in PROD!");
       return "Greetings from Spring Boot 2x!";
     }
 
